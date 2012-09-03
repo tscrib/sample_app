6e91b57a099d205c6eefb78b879c44ee06b87a0a
Tim Scribner <tim.scribner@gmail.com>
2012-09-03 18:26:48 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,14 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
 
+
+	# Public Function Definitions
+	def feed
+	    # This is preliminary. See "Following users" for the full implementation.
+	    Micropost.where("user_id = ?", id)
+	end
+
+
 	# Private Function Definitions
 	private
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,6 +25,22 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: "| Home") }
 
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
+
   end
 
   # Test for the help_path page
@@ -62,19 +78,19 @@ describe "Static pages" do
 
   # Test the links
   describe "Link Test" do
-     it "should have the right links on the layout" do
-      visit root_path
-      click_link "About"
-      page.should have_selector 'title', text: full_title('About Us')
-      click_link "Help"
-      page.should have_selector 'title', text: full_title('Help')
-      click_link "Contact"
-      page.should have_selector 'title', text: full_title('Contact')
-      click_link "Home"
-      click_link "Sign up now!"
-      page.should have_selector 'title', text: full_title('Sign Up')
-      click_link "sample app"
-      page.should have_selector 'title', text: full_title('')
-    end
+   it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign Up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
   end
+end
 end
