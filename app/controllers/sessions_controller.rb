09cb18cda1c71ca0bdef8439cbcf201a6cae7a9f
Tim Scribner <tim.scribner@gmail.com>
2012-09-03 00:51:11 -0400

    Refactor tests to be more clear

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,10 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:session][:email].downcase)
+		user = User.find_by_email(params[:email].downcase)
 
-		if user && user.authenticate(params[:session][:password])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
 			sign_in user
 			redirect_to user
 		else
